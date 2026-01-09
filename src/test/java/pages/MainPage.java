298f7fec1d57d4ebc95a679d246231422fa70966
vienpham <vienpham268@gmail.com>
2026-01-09 07:43:34 +0700

    config to get @step of allure

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,6 +3,7 @@ package pages;
 import base.BasePage;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
@@ -25,12 +26,14 @@ public class MainPage extends BasePage {
     @AndroidFindBy(id = "android:id/message")
     private WebElement txtMessage;
 
+    @Step("Dialing number 1 and making a call")
     public void dialAndCall() {
         btnDialPad.click();
         btnOne.click();
         btnCall.click();
     }
 
+    @Step("Verifying dial message after pressing Call")
     public void verifyDialMessage() {
         this.waitForVisible(txtMessage);
         Assert.assertEquals(txtMessage.getText(), "USSD code running…");
